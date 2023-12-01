22cdef65d851f811b2827e0fb89cff56472b30f5
Lee Smet <lee.smet@hotmail.com>
2023-12-01 17:33:20 +0100

    Add solution for day 1 challange 1
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/bin/d_1_1.rs
+++ src/bin/d_1_1.rs
@@ -1,3 +1,24 @@
+/// Input file included in binary to avoid runtime file IO.
+const INPUT_FILE: &str = include_str!("../../assets/d_1_1.txt");
+
 fn main() {
-    println!("It works");
+    let total: u32 = INPUT_FILE
+        .lines()
+        .map(|line| {
+            let mut nums = line.chars().filter(char::is_ascii_digit);
+            let first = nums.next();
+            let last = nums.last();
+
+            match (first, last) {
+                (Some(first), Some(last)) => {
+                    first.to_digit(10).unwrap() * 10 + last.to_digit(10).unwrap()
+                }
+                (Some(first), None) => first.to_digit(10).unwrap() * 11,
+                (None, None) => 0,
+                (None, Some(_)) => unreachable!(),
+            }
+        })
+        .sum();
+
+    println!("Total calibration values sum is {total}");
 }
